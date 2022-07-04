1e458af3aa601f2e3fb67f6aca05a3e10a293c00
Teobaldo Diaz <godofredo.diaz@tecsup.edu.pe>
2022-07-04 15:23:12 -0500

    Semana 01

---
+++ semana_01/10_String..py
@@ -0,0 +1,13 @@
+'''
+Programa: Fundamentos de Python
+Sesión: 01: Introducción
+Tema: String.
+Fecha:072022
+Versión: 1.1
+Autor: Godofredo Diaz.
+'''
+
+#Expresiones que utilizen la estructura: ax+bj
+#Contiene una parte real y otra imaginaria.
+# j es la raíz cuadrada de -1
+
